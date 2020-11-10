151b22979d7296862543ba15568fb896cca3576c
Maja Francetic <francetic.maja@gmail.com>
2020-11-10 10:25:42 +0100

    Implement data binding in the project, and use it to inflate the content view of the MainActivity

--- starter/app/src/main/java/com/mfrancetic/bookstore/MainActivity.kt
+++ starter/app/src/main/java/com/mfrancetic/bookstore/MainActivity.kt
@@ -2,13 +2,19 @@ package com.mfrancetic.bookstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.mfrancetic.bookstore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         Timber.plant(Timber.DebugTree())
     }
 }
