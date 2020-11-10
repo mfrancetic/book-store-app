5f5c20d41ae8e9ae439ce8748cd731892a42ec5e
Maja Francetic <francetic.maja@gmail.com>
2020-11-10 14:54:31 +0100

    Replace the FragmentContainerView in the activity_main.xml to fragment; setup the NavController with the toolbar and AppBarConfiguration in the MainActivity; update labels for the fragments

--- starter/app/src/main/java/com/mfrancetic/bookstore/MainActivity.kt
+++ starter/app/src/main/java/com/mfrancetic/bookstore/MainActivity.kt
@@ -3,18 +3,42 @@ package com.mfrancetic.bookstore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.mfrancetic.bookstore.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        title = getString(R.string.app_name)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        setupNavControllerAndToolbar()
+
         Timber.plant(Timber.DebugTree())
+    }
+
+    private fun setupNavControllerAndToolbar() {
+        navController = this.findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupWithNavController(binding.toolbar, navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
--- starter/app/src/main/java/com/mfrancetic/bookstore/MainViewModel.kt
+++ starter/app/src/main/java/com/mfrancetic/bookstore/MainViewModel.kt
@@ -1,4 +1,4 @@
-package com.mfrancetic.bookstore.screens
+package com.mfrancetic.bookstore
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
