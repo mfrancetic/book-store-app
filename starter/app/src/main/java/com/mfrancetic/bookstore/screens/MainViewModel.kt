a1e629675262ca9fb63effc7ca13bc9219f79d59
Maja Francetic <francetic.maja@gmail.com>
2020-11-10 13:05:52 +0100

    Create MainViewModel class with private and public LiveData variables that hold a book list

---
+++ starter/app/src/main/java/com/mfrancetic/bookstore/screens/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.mfrancetic.bookstore.screens
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.mfrancetic.bookstore.models.Book
+
+class MainViewModel : ViewModel() {
+
+    private val _books: MutableLiveData<List<Book>> = MutableLiveData()
+    private val books: LiveData<List<Book>>
+        get() = _books
+}
